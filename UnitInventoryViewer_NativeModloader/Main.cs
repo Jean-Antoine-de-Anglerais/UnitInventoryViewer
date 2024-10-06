3b2f218b0f18598d5db63866e9d7ba34391aafc5
Jean-Antoine d'Anglerais <zzzzzzzorro@mail.ru>
2024-10-06 20:32:37 +0300

    Добавьте файлы проекта.

---
+++ UnitInventoryViewer_NativeModloader/Main.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace UnitInventoryViewer_NativeModloader
+{
+    internal class Main : MonoBehaviour
+    {
+        public static Harmony harmony = new Harmony(MethodBase.GetCurrentMethod().DeclaringType.Namespace);
+        private bool _initialized = false;
+
+        public void Update()
+        {
+            if (global::Config.gameLoaded && !_initialized)
+            {
+                harmony.Patch(AccessTools.Method(typeof(WindowCreatureInfo), nameof(WindowCreatureInfo.OnEnable)),
+                postfix: new HarmonyMethod(AccessTools.Method(typeof(Patches), nameof(Patches.OnEnable_Postfix))));
+
+                _initialized = true;
+            }
+        }
+    }
+}
---
+++ UnitInventoryViewer_NativeModloader/Patches.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UI;
+
+namespace UnitInventoryViewer_NativeModloader
+{
+    public static class Patches
+    {
+        public static void OnEnable_Postfix(WindowCreatureInfo __instance)
+        {
+            if (__instance.actor.inventory != null && __instance.actor.inventory.getResources().Count > 0) 
+            {
+                __instance.showCustomStat("resources", __instance.actor.inventory.getResources());
+            }
+        }
+
+        public static void showCustomStat(this WindowCreatureInfo instance, string pID, Dictionary<string, ResourceContainer> dict)
+        {
+            Text text = instance.text_description;
+            text.text = text.text + LocalizedTextManager.getText(pID) + "\n";
+            Text text2 = instance.text_values;
+            text2.text = text2.text + ((dict != null) ? string.Join(", ", dict.Select(r => $"{LocalizedTextManager.getText(r.Value.id)}: {r.Value.amount}")) : null) + "\n";
+        }
+    }
+}
