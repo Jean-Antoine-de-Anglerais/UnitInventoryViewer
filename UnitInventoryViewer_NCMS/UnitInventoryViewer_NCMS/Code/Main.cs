cc4c93aaff32f2b72d35b9fe6ec1a177fd34ef92
Jean-Antoine d'Anglerais <zzzzzzzorro@mail.ru>
2024-10-07 19:15:32 +0300

    Added NCMS/NML version of the mod

---
+++ UnitInventoryViewer_NCMS/UnitInventoryViewer_NCMS/Code/Main.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using NCMS;
+using System.Reflection;
+using UnityEngine;
+
+namespace UnitInventoryViewer_NCMS
+{
+    [ModEntry]
+    class Main : MonoBehaviour
+    {
+        public static Harmony harmony = new Harmony(MethodBase.GetCurrentMethod().DeclaringType.Namespace);
+
+        void Awake()
+        {
+            harmony.Patch(AccessTools.Method(typeof(WindowCreatureInfo), nameof(WindowCreatureInfo.OnEnable)),
+            postfix: new HarmonyMethod(AccessTools.Method(typeof(Patches), nameof(Patches.OnEnable_Postfix))));
+        }
+    }
+}
